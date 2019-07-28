abe0a279ef78757f06abc57b060afc0385c5925f
Timothy Grindall <tim.grindall@gmail.com>
2019-07-27 17:59:37 -0700

    delisted build directory from .gitignore

---
+++ build/precache-manifest.a1357f2981dd649e5fd9fede52710dbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7f4d7e6f4ec92621a4ec08b492239a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7b2c97839434e81945f",
+    "url": "/static/css/main.5aa395be.chunk.css"
+  },
+  {
+    "revision": "80d12d18de653df7edee",
+    "url": "/static/js/2.673dccd0.chunk.js"
+  },
+  {
+    "revision": "b7b2c97839434e81945f",
+    "url": "/static/js/main.38ced8e8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
